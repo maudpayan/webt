2ddcdc31778b2d83f84e52e1277a3cb31f60be19
Maud Payan <maudpayan@gmail.com>
2019-04-14 16:08:34 -0700

    navbar+footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :product, :about, :contact, :demo]
 
   def home
   end
+
+  def product
+  end
+
+  def about
+  end
+
+  def demo
+  end
+
+  def contact
+  end
+
 end
