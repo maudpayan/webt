dc24f39f52ed4f6f15ef3dfc7a72855b11b209ac
Maud Payan <maudpayan@gmail.com>
2019-04-15 18:01:04 -0700

    ok

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,29 +24,3 @@ class MessagesController < ApplicationController
   end
 
 end
-
-# class MessagesController < ApplicationController
-#   skip_before_action :authenticate_user!, only: [:new, :create]
-
-#   def new
-#     @message = Message.new
-#   end
-
-#   def create
-#     @message = Message.new message_params
-
-#     if @message.valid?
-#       MessageMailer.contact_me(@message).deliver_now
-#       redirect_to new_message_path, notice: "Message received"
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def message_params
-#     params.require(:message).permit(:name, :email, :company, :body)
-#   end
-# end
-
